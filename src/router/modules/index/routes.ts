29f2a9af9f820464eeda6384812c55bc8da771db
xiaoyang <2533037186@qq.com>
2024-03-14 09:07:10 +0800

    xy: 路由备注

--- src/router/modules/index/routes.ts
+++ src/router/modules/index/routes.ts
@@ -1,12 +1,13 @@
 export default {
-	label: "首页",
-	path: '/',
-	key: '/',
-	component: () => import("@views/index/index.vue"),
+	label: "首页", // 路由名称
+	path: '/', // router路径
+	key: '/', // menu组件地址
+	component: () => import("@views/index/index.vue"), // 组件引用
+	icon: "", // menu组件的图标，这里还需要完善
 	meta: {
 		title: "首页"
 	},
-	show: true,
-	disabled: false,
-	// children: []
+	show: true, // 是否显示
+	disabled: false, // 是否禁用
+	// children: [] //
 };
