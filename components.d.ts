348dce114d79ac1a823b0b25affbe5d91191187d
xiaoyang <2533037186@qq.com>
2024-03-15 15:56:39 +0800

    xy: tabs布局

--- components.d.ts
+++ components.d.ts
@@ -8,16 +8,12 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
-    NButton: typeof import('naive-ui')['NButton']
-    NCard: typeof import('naive-ui')['NCard']
     NLayout: typeof import('naive-ui')['NLayout']
     NLayoutContent: typeof import('naive-ui')['NLayoutContent']
     NLayoutFooter: typeof import('naive-ui')['NLayoutFooter']
     NLayoutHeader: typeof import('naive-ui')['NLayoutHeader']
     NLayoutSider: typeof import('naive-ui')['NLayoutSider']
     NMenu: typeof import('naive-ui')['NMenu']
-    NModal: typeof import('naive-ui')['NModal']
-    NResult: typeof import('naive-ui')['NResult']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,9 +1,67 @@
 <template>
-    123
+    <div class="index">
+        <div class="top">
+            <div class="one">TAB1</div>
+            <div class="two">TAB2</div>
+            <div class="three">TAB3</div>
+            <div class="infine"></div>
+        </div>
+        <div class="bottom">
+            <div class="title">THIS IS TAB1 CONTENT</div>
+            <div class="content">Notice the gap between the content and tab after applying a background color</div>
+        </div>
+    </div>
 </template>
 
 <script setup lang="ts">
 import LayoutView from '@/layout/index.vue'
 </script>
 
-<style></style>
+<style lang="less" scoped>
+.index {
+    display: flex;
+    flex-direction: column;
+    .top {
+        display: flex;
+        font-weight: bold;
+        .one,
+        .two,
+        .three {
+            color: rgb(75,141,199);
+            // border: 1px solid;
+            padding: 10px 20px;
+            // width: ;
+            // margin-right: 10px;
+            border-top-left-radius: 4px;
+            border-top-right-radius: 4px;
+        }
+        .one {
+            border: 1px solid rgb(230, 187, 187);
+            border-bottom: none;
+            color: black;
+        }
+        .two,
+        .three {
+            border-bottom: 1px solid rgb(230, 187, 187);
+        }
+        .infine {
+            width: 100%;
+            border-bottom: 1px solid rgb(230, 187, 187);
+        }
+        // border-bottom: 1px solid #000;
+        margin-bottom: 20px;
+    }
+    .bottom {
+        margin-left: 10px;
+        font-size: 24px;
+        font-weight: bold;
+        color: rgb(75,141,199);
+        .title {
+            margin-bottom: 10px;
+        }
+        .content {
+            width: 482px;
+        }
+    }
+}
+</style>
